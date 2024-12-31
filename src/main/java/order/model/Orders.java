1e8b12f853bb507962ab7b7a1de24287313aa196
kangrae <kangrae0501@gmail.com>
2024-12-31 13:24:50 +0900

    fix: 주문 객체를 검증하던 클래스 삭제
    - 주문 객체를 담는 Orders 클래스에서 유효성 검사를 함께 실행함
    
    - 코드 응집도 증가, 외부 코드 간결화, 일급 컬렉션 특징 강화

--- src/main/java/order/model/Orders.java
+++ src/main/java/order/model/Orders.java
@@ -3,9 +3,33 @@ package order.model;
 import java.util.List;
 import order.constant.Menu;
 
-public class OrderValidator {
+public class Orders {
 
-    public void ordersCheck(List<Order> orders) throws IllegalArgumentException {
+    private final List<Order> orders;
+
+    public Orders(List<Order> orders) {
+        this.orders = orders;
+        validateOrders();
+    }
+
+    public int countMainMenu() {
+        return (int) orders.stream()
+                .filter(Order::isMain)
+                .mapToLong(Order::getQuantity)
+                .sum();
+    }
+
+    public int getTotalPrice() {
+        return orders.stream()
+                .mapToInt(Order::getPrice)
+                .sum();
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    private void validateOrders() {
         for (Order order : orders) {
             if (!isExistMenu(order)) {
                 throw new IllegalArgumentException("존재하지 않는 메뉴입니다.");
@@ -14,10 +38,10 @@ public class OrderValidator {
                 throw new IllegalArgumentException("한 번에 0 ~ 10개만 주문 가능합니다.");
             }
         }
-        if (!isValidAmount(orders)) {
-            throw new IllegalArgumentException("주문 최소 금액은 30,000원 입니다.");
+        if (!isValidAmount()) {
+            throw new IllegalArgumentException("주문 최소 금액을 만족하지 못했습니다.");
         }
-        if (isOnlyDrink(orders)) {
+        if (isOnlyDrink()) {
             throw new IllegalArgumentException("음료만으로는 주문할 수 없습니다.");
         }
     }
@@ -30,15 +54,14 @@ public class OrderValidator {
         return order.isLessThanTen();
     }
 
-    private boolean isValidAmount(List<Order> orders) {
+    private boolean isValidAmount() {
         int totalAmount = orders.stream()
                 .mapToInt(order -> Menu.getPriceByName(order.getName()) * order.getQuantity())
                 .sum();
         return totalAmount >= 30_000;
     }
 
-    private boolean isOnlyDrink(List<Order> orders) {
-        return orders.stream()
-                .allMatch(Order::isDrink);
+    private boolean isOnlyDrink() {
+        return orders.stream().allMatch(Order::isDrink);
     }
 }
